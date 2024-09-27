2fbe6cc405258bb9ff582af81c4f431ef84248b6
mohammedoh87 <aamina.oh87@gmail.com>
2024-09-27 16:21:24 +0300

    Arguments ACP

---
+++ Shutdown.py
@@ -0,0 +1,12 @@
+def shutdown(choice):
+    choice = input("o you want to shutdown your computer: ")
+    if choice == "Yes".lower():
+        print("Shutdown")
+    elif choice == "No".lower():
+        print("Abort Shutdown")
+    else:
+        print("INVALID CHOICE")
+
+shutdown('choice')
+    
+    
